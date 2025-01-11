e39c2ac6ff74d94dceaca127c4f1371c7e108db5
AngiBF18 <angelica.blanco@correo.unimet.edu.ve>
2025-01-11 09:58:59 -0400

    Add files via upload

---
+++ Tarea2 (Arepas Dinámicas).py
@@ -0,0 +1,27 @@
+print ("Receta para hacer una arepa")
+# Imgredientes para preparar una arepa 
+harina_PAN = ("1. Harina PAN")
+agua = ("2. Agua")
+aceite = ("3. Aceite")
+sal = ("4. Sal")
+
+print(harina_PAN)
+print(agua)
+print(aceite)
+print (sal)
+
+print ("Ingrese las cantidades a utilizar")
+
+# Cantidades deseadas
+h = int(input("Tazas de Harina:"))
+a= int(input("Tazas de Agua:"))
+A= float (input("Cucharadas de Aceite:"))
+s= float (input("Cucharadas de Sal:"))
+
+ #Valor total 
+Bowl= a+h+s
+Sarten= Bowl+A
+
+#Volúmen total 
+print("Esta es su cantidad total:", Sarten)
+
